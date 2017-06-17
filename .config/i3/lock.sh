85857965e7e59e6f7d17b0bcf56280772120366c
Brian Lamere <blamere@gmail.com>
2017-06-17 15:14:38 -0700

    Added a lock script wrapper for i3lock.

---
+++ .config/i3/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+scrot /tmp/screen.png
+# convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -scale 25% -scale 400% /tmp/screen.png
+[[ -f $1 ]] && convert $1 /tmp/screen-overlay.png && convert /tmp/screen.png /tmp/screen-overlay.png -gravity center -composite -matte /tmp/screen.png
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
+rm /tmp/screen-overlay.png
