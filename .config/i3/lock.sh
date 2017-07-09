f9b85a565bc927de558b1c4712e5a8892a23ce73
Brian Lamere <lamereb@oregonstate.edu>
2017-07-08 21:36:07 -0700

    Changed a font; enabled the i3lock indicator

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -8,6 +8,6 @@ convert $SCREEN -scale 5% -scale 2000% $SCREEN
 [[ -f $1 ]] && ORIG_LOCK=$1
 convert $ORIG_LOCK -scale 200% -alpha set -channel a -evaluate set 85% +channel $LOCK
 convert $SCREEN $LOCK -gravity center -composite -matte $SCREEN
-i3lock -u -i $SCREEN
+i3lock -i $SCREEN
 rm $SCREEN
 rm $LOCK
