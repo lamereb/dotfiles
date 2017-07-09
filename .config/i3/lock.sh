aea0272c8cdda5004106985703e3ef6bfc9b3efa
Brian Lamere <lamereb@oregonstate.edu>
2017-07-09 12:06:23 -0700

    Changed i3lock overlay picture

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 SCREEN=/tmp/screen.png
 LOCK=/tmp/screen-overlay.png
-ORIG_LOCK=/home/lamereb/Pictures/desktop/encom-tron.jpg
+# ORIG_LOCK=/home/lamereb/Pictures/desktop/encom-tron.jpg
+ORIG_LOCK=/home/lamereb/Pictures/desktop/nKxrAY9.jpg
 
 scrot $SCREEN
 convert $SCREEN -scale 5% -scale 2000% $SCREEN
 [[ -f $1 ]] && ORIG_LOCK=$1
-convert $ORIG_LOCK -scale 200% -alpha set -channel a -evaluate set 85% +channel $LOCK
+# convert $ORIG_LOCK -scale 200% -alpha set -channel a -evaluate set 85% +channel $LOCK
+convert $ORIG_LOCK -scale 100% -alpha set -channel a -evaluate set 85% +channel $LOCK
 convert $SCREEN $LOCK -gravity center -composite -matte $SCREEN
 i3lock -i $SCREEN
 rm $SCREEN
